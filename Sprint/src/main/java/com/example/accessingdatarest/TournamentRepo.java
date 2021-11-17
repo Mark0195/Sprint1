f966e8988b684a9edefb40535af9cac4d0437bc5
Mark0195 <mark.myles@keyin.com>
2021-11-17 11:39:30 -0330

    updating files to connect to database

--- Sprint/src/main/java/com/example/accessingdatarest/TournamentRepo.java
+++ Sprint/src/main/java/com/example/accessingdatarest/TournamentRepo.java
@@ -6,11 +6,11 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "Tournaments", path = "Tournaments")
+@RepositoryRestResource(collectionResourceRel = "tournament", path = "tournament")
 
 public interface TournamentRepo extends PagingAndSortingRepository<Tournament, Long> {
 
-    List<Tournament> findByNextTournament(@Param("Start") String Start);
+    List<Tournament> findByStartOfTournament(@Param("Start") String Start);
 
 }
 
