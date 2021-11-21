c425aa5ae5960eccccfae90862780b8a1ddfe507
Mark0195 <mark.myles@keyin.com>
2021-11-21 10:34:28 -0330

    Added Person class/repo/controller

--- Sprint/src/main/java/com/example/repos/PersonRepo.java
+++ Sprint/src/main/java/com/example/repos/PersonRepo.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "People", path = "People")
 public interface PersonRepo extends PagingAndSortingRepository<Person, Long> {
     List<Person> findByFirstName(@Param("FirstName") String FirstName);
     List<Person> findByLastName(@Param("LastName") String LastName);
